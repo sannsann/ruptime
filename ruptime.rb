6b1a29e7e27e8d32eb3a7950b069f29fe6e89d99
Sann Chhan <sann.c.chhan@gmail.com>
2016-04-08 16:11:38 -0700

    Add error capture. Add first batch of unit tests.

--- ruptime.rb
+++ ruptime.rb
@@ -22,69 +22,96 @@ opt_parser = OptionParser.new do |opt|
   end
 end
 
-def uptime_json
-  uptime = `uptime`
-  uptimeArray = uptime.chomp.split(", ")
-
-  up_days = 0
-  up_hours = 0
-  up_minutes = 0
-
-  # resultArray.length will change depending on the the actual uptime duration. See notes below
-  # If the system has been on for longer than 24 hours:
-  # => number of days take place of the hh:mm
-  # => hh:mm gets separated into a subsequent array entry
-  check_time = uptimeArray[0].split(" ")[0]
-
-  # Get the load averages
-  load_avgs = uptimeArray[-1].split(": ")[1].split(" ").map{ |i| i.to_f.to_s }
-
-  # Get the number of users
-  num_users = uptimeArray[-2].split(" ")[0]
-
-# Notes:
-# Different cases where the units of time vary
-# 13:10  up 13 days,  4:30, 3 users, load averages: 1.29 1.34 1.33
-# 15:51  up  2:37, 3 users, load averages: 14.40 8.22 4.08
-# 13:15  up 30 secs, 3 users, load averages: 3.89 0.96 0.35
-# 13:15  up 1 min, 3 users, load averages: 2.60 1.06 0.42
+def uptime_json(*input)
   
-  # uptimeArray will vary in length depending on uptime reported
-  if (uptimeArray.length == 3)
-    # Case uptime has not exceeded 24 hours / 1 day
-    up_info_array = uptimeArray[0].split(" ") # Contains uptime info until the first comma from uptime
-
-    if (up_info_array.include?("min")) # Only minutes have been outputted.
-      up_minutes = up_info_array[-2]
-    elsif (up_info_array.find { |e| /sec/ =~ e })  # Corner case where uptime is executed before minute mark
-      up_minutes = 1 # Round uptime to 1 minute. We can update this if greater granularity is required for seconds
-    else # up_info_array contains uptime in Hours and minutes outputted as hh:mm
-      up_hours = up_info_array[2].split(":")[0]
-      up_minutes = up_info_array[2].split(":")[1]
-    end
-
-  elsif (uptimeArray.length == 4)
-    # Case where uptime exceeds 24 hours
-    up_days = up_info_array[2]
+  if input.empty?
+    uptime = `uptime`
   else
-    puts "We've encountered an unexpected scenario. Please email sann.c.chhan@gmail.com for assistance."
-    exit
+    uptime = input[0]
   end
 
+  # Check if there was an empty result form `uptime` invoke. May occur if the ruptime.rb is not run on a UNIX platform
+  if (uptime.strip.empty? || uptime.nil?)
+    return -1
+  end
 
-  # Create the JSON with a heredoc
-  json = <<EOS
+  begin
+    uptimeArray = uptime.chomp.split(", ")
+
+    up_days = 0
+    up_hours = 0
+    up_minutes = 0
+
+    # resultArray.length will change depending on the the actual uptime duration. See notes below
+    # If the system has been on for longer than 24 hours:
+    # => number of days take place of the hh:mm
+    # => hh:mm gets separated into a subsequent array entry
+    check_time = uptimeArray[0].split(" ")[0]
+
+    # Get the load averages
+    load_avgs = uptimeArray[-1].split(": ")[1].split(" ").map{ |i| i.to_f.to_s }
+
+    # Get the number of users
+    num_users = uptimeArray[-2].split(" ")[0]
+
+  # Notes:
+  # Different cases where the units of time vary
+  # 13:10  up 13 days,  4:30, 3 users, load averages: 1.29 1.34 1.33
+  # 13:17  up 1 day, 3 mins, 4 users, load averages: 1.51 1.21 1.19
+  # 15:51  up  2:37, 3 users, load averages: 14.40 8.22 4.08
+  # 13:15  up 30 secs, 3 users, load averages: 3.89 0.96 0.35
+  # 13:15  up 1 min, 3 users, load averages: 2.60 1.06 0.42
+    up_info_array = uptimeArray[0].split(" ") # Contains uptime info until the first comma from uptime
+    
+    # uptimeArray will vary in length depending on uptime reported
+    if (uptimeArray.length == 3)
+      # Case uptime has not exceeded 24 hours / 1 day
+      if (up_info_array.find { |e| /min/ =~ e }) 
+      # Only minutes have been outputted.
+        up_minutes = up_info_array[-2]
+      elsif (up_info_array.find { |e| /sec/ =~ e })  
+      # Corner case where uptime is executed before minute mark
+      # Round uptime to 1 minute. We can update this if greater granularity is required for seconds
+        up_minutes = 1 
+      else # up_info_array contains uptime in hours and minutes outputted as hh:mm
+        up_hours = up_info_array[2].split(":")[0]
+        up_minutes = up_info_array[2].split(":")[1]
+      end
+
+    elsif (uptimeArray.length == 4)
+      # Case where uptime exceeds 24 hours
+      up_days = up_info_array[2]
+
+      if (uptimeArray[1].split.find{ |e| /:/ =~ e })
+        up_hours = uptimeArray[1].split(":")[0].strip
+        up_minutes = uptimeArray[1].split(":")[1].strip
+      elsif (uptimeArray[1].split.find{ |e| /min/ =~ e })
+        up_minutes = uptimeArray[1].split(" ")[0]
+      end
+
+    end
+  rescue Exception => e
+    # puts "Please email sann.c.chhan@gmail.com for assistance."
+    # puts "Please include the following in the mail body: "
+    # puts
+    puts "contents of uptime: #{uptime}"
+    # puts
+    return -1
+  end
+
+    # Create the JSON with a heredoc
+    json = <<EOS
 {
-  checkTime: "#{check_time}",
-  days: "#{up_days}",
-  hours: "#{up_hours}",
-  minutes: "#{up_minutes}"
-  users: "#{num_users}",
-  loads: #{load_avgs}
+  "checkTime": "#{check_time}",
+  "days": "#{up_days}",
+  "hours": "#{up_hours}",
+  "minutes": "#{up_minutes}",
+  "users": "#{num_users}",
+  "loads": #{load_avgs}
 }
 EOS
-
-  json
+   
+    json
 end
 
 opt_parser.parse! 
---
+++ tc_ruptime.rb
@@ -0,0 +1,75 @@
+require_relative "ruptime"
+require 'json'
+
+puts "Case: uptime invokation returns an empty response from the shell. Error caught."
+uptime = uptime_json("")
+puts uptime == -1
+puts
+
+puts "Case: uptime invokation returns a non-standard response from the shell. Error caught."
+uptime = uptime_json("No command 'uptime' recognized.")
+puts uptime == -1
+puts
+
+puts "Case: uptime exceeds 1 day."
+# Setup
+uptime = uptime_json("13:10  up 13 days,  4:30, 3 users, load averages: 1.29 1.34 1.33")
+result = JSON.parse(uptime)
+# Tests
+puts result["checkTime"] == "13:10" 
+puts result["days"] == "13"
+puts result["hours"] == "4"
+puts result["minutes"] == "30"
+puts result["users"] == "3"
+puts result["loads"].eql? ["1.29", "1.34", "1.33"]
+puts
+
+puts "Case: uptime does not exceed 1 day."
+# Setup
+uptime = uptime_json("15:51  up  2:37, 3 users, load averages: 14.40 8.22 4.08")
+result = JSON.parse(uptime)
+
+# Tests
+puts result["days"] == "0"
+puts result["hours"] == "2"
+puts result["minutes"] == "37"
+
+# Testing incorrect results
+puts result["days"] != "1"
+puts
+
+puts "Case: uptime does not exceed one hour. Has minute values"
+uptime = uptime_json("13:15  up 1 min, 3 users, load averages: 2.60 1.06 0.42")
+result = JSON.parse(uptime)
+
+# Tests
+puts result["days"] == "0"
+puts result["hours"] == "0"
+puts result["minutes"] == "1"
+
+# Testing incorrect results
+puts result["days"] != "1"
+puts result["hours"] != "10"
+puts
+
+puts "Case: uptime does not exceed 1 minute. The key minute will output to nearest full minute."
+# puts "Testing case: 13:15  up 30 secs, 3 users, load averages: 3.89 0.96 0.35"
+uptime = uptime_json("13:15  up 30 secs, 3 users, load averages: 3.89 0.96 0.35")
+result = JSON.parse(uptime)
+
+# Tests
+puts result["minutes"] == "1"
+
+# Test incoreect results
+puts result["minutes"] != "0"
+puts
+
+puts "Case: uptime outputs to file - file name unspecified"
+`ruby ruptime.rb -f`
+puts File.exist?('./ruptime.json')
+puts
+
+puts "Case: uptime outputs to file - file name specified"
+`ruby ruptime.rb -f test.json`
+puts File.exist?('./test.json')
+puts !File.exist?('./text.json')
